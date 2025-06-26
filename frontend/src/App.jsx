cd89d1ff123c404e37ff3e76b537c255202329de
godbrand <thompsoneregha005@gmail.com>
2025-06-26 20:33:24 +0100

    Removed ESLint configuration and React Router setup from frontend codebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
