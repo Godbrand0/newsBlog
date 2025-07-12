ac7f3cf08cdeb6dc9821ff19d702ff6972663ded
Thompson <140930314+Godbrand0@users.noreply.github.com>
2025-07-12 18:26:55 +0100

    changes in dotenv

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+
+import dotenv from "dotenv";
+dotenv.config({path: ".env"});
 const express = require("express");
 const mongoose = require("mongoose");
 const postRoutes = require("./routes/postRoutes");
