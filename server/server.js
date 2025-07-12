9befdbab0792dab84ea7c4af7e4006619dae37e3
Thompson <140930314+Godbrand0@users.noreply.github.com>
2025-07-12 18:44:07 +0100

    package changes

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ cloudinary.config({
 });
 
 const app = express();
-console.log(`.env file = ${process.env.MONGO_URI}]]`)
+console.log(`.env file = ${process.env.MONGO_URI}`)
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
